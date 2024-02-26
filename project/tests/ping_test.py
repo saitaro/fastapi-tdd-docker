6e448ac80880bd2e97870c952fbbd196aae63367
saitaro <nort87@mail.ru>
2024-02-26 05:58:55 +0300

    Fix test

--- project/tests/ping_test.py
+++ project/tests/ping_test.py
@@ -9,4 +9,4 @@ def test_ping(test_app: TestClient):
     response = test_app.get('/ping')
 
     assert response.status_code == 200
-    assert response.json() == {'environment': 'dev', 'testing': True, 'ping': 'pong!'}
+    assert response.json() == {'environment': 'dev', 'testing': True, 'ping': 'pong on Render!!!'}
